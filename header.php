7312f83c17c8b14e572e881c0210575d7ddca82b
yathaw <yathawmyatnoe007@gmail.com>
2019-11-05 10:27:29 +0630

    Form Design

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> JSON File Read Write </title>
+	
+	<!-- BOOTSTRAP CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+	<!-- JQ -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+
+	<!-- BOOTSTRAP JS -->
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+
---
+++ index.php
@@ -0,0 +1,220 @@
+<?php require "header.php"; ?>
+
+	
+	<div class="container" id="addStudentdiv">
+		
+		<div class="row mt-5">
+			<div class="col-12 text-center">
+				<h1 class="display-4"> Add New Student </h1>
+			</div>
+		</div>
+
+		<div class="row mt-5">
+			<div class="col align-self-center">
+				<form action="addstudent.php" method="POST" enctype="multipart/form-data">
+					<div class="form-group row">
+						<label for="profile" class="col-sm-2 col-form-label"> Profile </label>
+					    <div class="col-sm-10">
+					    	<input type="file"  id="profile" name="profile">
+					    </div>
+					</div>
+
+					<div class="form-group row">
+						<label for="name" class="col-sm-2 col-form-label"> Name </label>
+					    <div class="col-sm-10">
+					    	<input type="text" class="form-control" id="name" placeholder="Enter Name" name="name">
+					    </div>
+					</div>
+
+					<div class="form-group row">
+						<label for="name" class="col-sm-2 col-form-label"> Email </label>
+					    <div class="col-sm-10">
+					    	<input type="email" class="form-control" id="name" placeholder="Enter Email" name="email">
+					    </div>
+					</div>
+
+					<fieldset class="form-group">
+					    <div class="row">
+					    	<legend class="col-form-label col-sm-2 pt-0"> Gender </legend>
+					      
+					      	<div class="col-sm-10">
+					        
+					        	<div class="form-check">
+					          		<input class="form-check-input" type="radio" name="gender" id="male" value="Male" checked>
+					          		<label class="form-check-label" for="male">
+					            		Male
+					          		</label>
+					        	</div>
+					        
+					        	<div class="form-check">
+					          		<input class="form-check-input" type="radio" name="gender" id="female" value="Female">
+					          		<label class="form-check-label" for="female">
+					            		Female
+					          		</label>
+					        	</div>
+					        
+					      </div>
+					    </div>
+					</fieldset>
+
+					<div class="form-group row">
+						<label for="address" class="col-sm-2 col-form-label"> Address </label>
+					    <div class="col-sm-10">
+					    	<textarea class="form-control" rows="5" name="address"></textarea>
+					    </div>
+					</div>
+
+					<div class="form-group row">
+					    <div class="col-sm-10">
+					   		<button type="submit" class="btn btn-primary">
+					   			SAVE
+					   		</button>
+					    </div>
+					</div>
+
+
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<div class="container" id="editStudentdiv">
+		
+		<div class="row mt-5">
+			<div class="col-12 text-center">
+				<h1 class="display-4"> Edit Existing Student </h1>
+			</div>
+		</div>
+
+		<div class="row mt-5">
+			<div class="col align-self-center">
+				<form action="updatestudent.php" method="POST" enctype="multipart/form-data">
+					<input type="hidden" name="edit_id" id="edit_id">
+					<input type="hidden" name="edit_oldprofile" id="edit_oldphoto">
+					<div class="form-group row">
+						<label for="profile" class="col-sm-2 col-form-label"> Profile </label>
+					    <div class="col-sm-10">
+
+							<ul class="nav nav-tabs" id="myTab" role="tablist">
+	  							<li class="nav-item">
+	    							<a class="nav-link active" id="oldprofile-tab" data-toggle="tab" href="#oldprofile" role="tab" aria-controls="oldprofile" aria-selected="true"> Old Profile </a>
+	  							</li>
+	  
+		  						<li class="nav-item">
+		    						<a class="nav-link" id="newprofile-tab" data-toggle="tab" href="#newprofile" role="tab" aria-controls="newprofile" aria-selected="false"> New Profile</a>
+		  						</li>
+							</ul>
+							<div class="tab-content" id="myTabContent">
+								<div class="tab-pane fade show active" id="oldprofile" role="tabpanel" aria-labelledby="oldprofile-tab">
+									<img src="" id="showOldPhoto" class="img-fluid" width="100px" height="90px">
+								</div>
+							
+								<div class="tab-pane fade" id="newprofile" role="tabpanel" aria-labelledby="newprofile-tab">
+									<input type="file"  id="profile" name="edit_newprofile">
+								</div>
+							</div>
+
+						
+					    	
+					    </div>
+					</div>
+
+					<div class="form-group row">
+						<label for="edit_name" class="col-sm-2 col-form-label"> Name </label>
+					    <div class="col-sm-10">
+					    	<input type="text" class="form-control" id="edit_name" placeholder="Enter Name" name="edit_name">
+					    </div>
+					</div>
+
+					<div class="form-group row">
+						<label for="edit_email" class="col-sm-2 col-form-label"> Email </label>
+					    <div class="col-sm-10">
+					    	<input type="email" class="form-control" id="edit_email" placeholder="Enter Email" name="edit_email">
+					    </div>
+					</div>
+
+					<fieldset class="form-group">
+					    <div class="row">
+					    	<legend class="col-form-label col-sm-2 pt-0"> Gender </legend>
+					      
+					      	<div class="col-sm-10">
+					        
+					        	<div class="form-check">
+					          		<input class="form-check-input" type="radio" name="edit_gender" id="edit_male" value="Male">
+					          		<label class="form-check-label" for="edit_male">
+					            		Male
+					          		</label>
+					        	</div>
+					        
+					        	<div class="form-check">
+					          		<input class="form-check-input" type="radio" name="edit_gender" id="edit_female" value="Female">
+					          		<label class="form-check-label" for="edit_female">
+					            		Female
+					          		</label>
+					        	</div>
+					        
+					      </div>
+					    </div>
+					</fieldset>
+
+					<div class="form-group row">
+						<label for="address" class="col-sm-2 col-form-label"> Address </label>
+					    <div class="col-sm-10">
+					    	<textarea class="form-control" rows="5" name="edit_address" id="edit_address"></textarea>
+					    </div>
+					</div>
+
+					<div class="form-group row">
+					    <div class="col-sm-10">
+					   		<button type="submit" class="btn btn-primary">
+					   			 Update
+					   		</button>
+					    </div>
+					</div>
+
+
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th> # </th>
+				<th> Name </th>
+				<th> Gender </th>
+				<th> Email </th>
+				<th> Action </th>
+			</tr>
+		</thead>
+		<tbody></tbody>
+	</table>
+
+	<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
+
+<!-- Detail Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php require "footer.php"; ?>
